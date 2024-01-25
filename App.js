2faa242abd110295e5672895dea89c9b8037462a
Pedro Chico <Pedro.Chico@one-beyond.com>
2024-01-25 10:21:53 +0100

    import amplify

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
